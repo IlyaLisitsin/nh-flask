60e15cfc25edaf38f6d6bffb855059cad585333a
illia <illias202030@gmail.com>
2023-05-12 01:30:25 +0300

    mock response

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def echo():
     data = request.json
     user_text = data['userText']
-    return jsonify({'echoedText': user_text})
+    list = ['1pdHpeJLP3rvbLyK6FkvNo','1JvJxRgnX0jgWvu1TIUOOh','0AVzrqkSr8JhJhzZ1$XluR']
+    return jsonify({'echoedText': user_text, 'ids': list, 'answerText': 'and at the end the love you make is eaten like a piece of cake'})
 
 if __name__ == '__main__':
     app.run()
