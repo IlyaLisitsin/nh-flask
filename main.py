85e05ca43a26e716e758b17a46d9bf53da544b35
navid <navid.kayhani@naska.ai>
2023-05-30 08:29:39 -0400

    Upddate Echo function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #%%  imports
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-
+import ast
 
 ###### AI #####################################################
 from langchain import OpenAI
@@ -29,15 +29,8 @@ llm = OpenAI(temperature=0)
 agent = create_csv_agent(llm, file_path, verbose=True)
 print('agent created')
 import prompt as pm
-agent.run(pm.prefix_csv_description)
-# user_text = 'select 50 random walls'
-# try:
-#     chatbot_text = agent.run(user_text)
-#     guid_list = agent.run(user_text + ' ' + pm.prefix_filtering)
-# except:
-#     chatbot_text = pm.error_message
-# print(agent.run('what is the most deviated element?'))
-# print(key)
+# agent.run(pm.prefix_csv_description)
+
 ###############################################################
 #%%  app 
 app = Flask(__name__)
@@ -51,14 +44,30 @@ def echo():
     user_text = data['userText']
     print(user_text)
     try:
-        chatbot_text = agent.run(user_text)
-        guid_list = agent.run(user_text + ' ' + pm.prefix_filtering)
+        # chatbot_text = agent.run(user_text)
+        # make user_text lowercase
+        if user_text.lower().startswith('select') or user_text.lower().startswith('show') or user_text.lower().startswith('display'):
+            guid_list = agent.run(pm.prefix_filtering + ' ' + user_text)
+            if '[' in guid_list and ']' in guid_list:
+                chatbot_text = guid_list.split('[')[0]
+                if chatbot_text == '':
+                    chatbot_text = 'I updated the viewer. :)'
+                guid_list = '[' + guid_list.split('[')[1].split(']')[0] + ']'
+            try:
+                guid_list = ast.literal_eval(guid_list)
+            except:
+                print(f'guid_list {guid_list} cannot be converted to a list')
+                pass
+        else:
+            chatbot_text = agent.run(user_text)
+            guid_list = []
     except:
         chatbot_text = pm.error_message
-    print(chatbot_text)
-    print(guid_list)
-    return jsonify({'echoedText': chatbot_text,
-                    'guidList': guid_list})
+        guid_list = []
+        
+    
+    return jsonify({'answerText': chatbot_text,
+                    'ids': guid_list})
 
 if __name__ == '__main__':
     app.run()
